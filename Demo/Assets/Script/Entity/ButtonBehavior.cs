f9bb0a887e1dd458e70f8d9fb276176f0db8c0d1
Synthesis-016 <72910142+Synthesis-016@users.noreply.github.com>
2021-01-20 11:49:00 +0800

    Add files via upload

---
+++ Demo/Assets/Script/Entity/ButtonBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject button;
+    public GameObject userinterface;
+    GameObject temp;
+    public Outline outline;
+    public Text text1, text2,text3;
+    public static bool isOn;
+    float width, height;
+    float posx, posy;
+    public void openInterface()
+    {
+        if (!isOn)
+        {
+            //userinterface=GameObject.Find("")
+            userinterface.SetActive(true);
+            isOn = true;
+        }
+    }
+    public void closeInterface()
+    {
+        if (isOn)
+        {
+            //userinterface=GameObject.Find("")
+            userinterface.SetActive(false);
+            isOn = false;
+        }
+    }
+    private void Start()
+    {
+        temp = GameObject.Find("Main Camera");
+        camera = temp.GetComponent<Camera>();
+        temp = null;
+        width = button.GetComponent<RectTransform>().rect.width;
+        height = button.GetComponent<RectTransform>().rect.height;
+        posx = camera.WorldToScreenPoint(button.transform.position).x;
+        posy = camera.WorldToScreenPoint(button.transform.position).y;
+        outline = button.GetComponent<Outline>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isOn)
+        {
+            if (Input.mousePosition.x < posx + width / 2 && Input.mousePosition.x > posx - width / 2 && Input.mousePosition.y < posy + height / 2 && Input.mousePosition.y > posy - height / 2)
+                outline.enabled = true;
+            else outline.enabled = false;
+            float a = posx + width / 2;
+            text1.text = Input.mousePosition.ToString();
+            text2.text = width.ToString();
+            text3.text = camera.WorldToScreenPoint(button.transform.position).ToString();
+            Debug.Log(Input.mousePosition + "  " + isOn);
+        }
+        else if (Input.GetKey(KeyCode.Escape))
+            closeInterface();
+        Debug.Log(isOn);
+    }
+}
---
+++ Demo/Assets/Script/Entity/pm.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class pm : MonoBehaviour
+{
+    int n=3;
+    GameObject temp;
+    public List<Image> image = new List<Image>();
+    public Text text;
+    void Awake()
+    {
+        
+        for(int i=0;i<n;i++)
+        {
+            image[i].color = new Color(0, 0, 0);
+        }
+        text.text = n.ToString();
+    }
+    public void plus()
+    {
+        n++;
+        if (n > 5) n = 5;
+        for (int i = 0; i < n; i++)
+        {
+            image[i].color = new Color(0, 0, 0);
+        }
+        text.text = n.ToString();
+    }
+    public void minus()
+    {
+        n--;
+        if (n < 0) n = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            image[i].color = new Color(255, 255, 255);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            image[i].color = new Color(0, 0, 0);
+        }
+        text.text = n.ToString();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
