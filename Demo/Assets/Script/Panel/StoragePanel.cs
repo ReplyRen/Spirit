08ac0b15998e74dfac26ad0609f4cbb110df5f41
Synthesis-016 <72910142+Synthesis-016@users.noreply.github.com>
2021-01-24 14:18:49 +0800

    更新storagepanel和UIobj

---
+++ Demo/Assets/Script/Panel/StoragePanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoragePanel : MonoBehaviour
+{
+    public List<BaseObject> storage = new List<BaseObject>();
+    //BaseObject card=new BaseObject();
+    public GameObject panel;
+    void Start()
+    {
+        int i;
+        for (i = 0; i < 10; i++)
+        {
+            BaseObject card = new BaseObject();
+            card.description = i.ToString();
+            storage.Add(card);
+        }
+        for(int j=0;j<i;j++)
+        {
+            Debug.Log(storage[j].description);
+        }
+    }
+    public void Add()
+    {
+        panel.SetActive(true);
+        for(int i=0;i<storage.Count;i++)
+        {
+            GameObject a = new GameObject();
+            Text text = a.AddComponent(typeof(Text)) as Text;
+            text.text = storage[i].description;
+            
+            //text.text = i.ToString();
+        }
+    }
+    void Update()
+    {
+        //Debug.Log(storage.Count);
+    }
+}
---
+++ Demo/Assets/Script/Panel/UIObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIObject:MonoBehaviour
+{
+    /// <summary>
+    /// “是否使用”属性
+    /// </summary>
+    public bool isUse;
+
+    /// <summary>
+    /// “是否确认”属性
+    /// </summary>
+    public bool isConfirm;
+}
