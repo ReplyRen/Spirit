1b29295fc25b7c9d8c0aa893735154d42583f678
Synthesis-016 <72910142+Synthesis-016@users.noreply.github.com>
2021-01-28 19:00:54 +0800

    更新调整碎片属性框架

---
+++ Demo/Assets/Script/Entity/ChangeData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeData
+{
+    public static void Acid(ref BaseFragment fragment)
+    {
+
+    }
+    public static void Ester(ref BaseFragment fragment)
+    {
+
+    }
+    public static void Alcohol(ref BaseFragment fragment)
+    {
+
+    }
+    public static void Microbe(ref BaseFragment fragment)
+    {
+
+    }
+    public static void Yield(ref BaseFragment fragment)
+    {
+
+    }
+    public static void Taste(ref BaseFragment fragment)
+    {
+
+    }
+    public static void AdvancedAcid(ref BaseFragment fragment)
+    {
+
+    }
+    public static void AdvancedEster(ref BaseFragment fragment)
+    {
+
+    }
+    public static void AdvancedAlcohol(ref BaseFragment fragment)
+    {
+
+    }
+}
