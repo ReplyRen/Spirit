785ff76fd96c1c8830e42799d2e24ab28849a2dd
bli23-01 <hust_hjj@126.com>
2021-01-31 15:39:15 +0800

    初始化圆盘和隐藏

--- Demo/Assets/Script/Panel/RoundPanel.cs
+++ Demo/Assets/Script/Panel/RoundPanel.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class RoundPanel : BasePanel
 {
-    private List<BaseFragment> testFragments;
+    private bool ifFirstTime = true;
+    /// <summary>
+    /// 流程里面把Start隐藏掉直接调用下面两个就行了
+    /// 把testFragments也隐藏掉把
+    /// </summary>
     void Start()
     {
-        Round.InitialAngle();FragmentsManager.InitialFragments();
+        List<BaseFragment> testFragments;
+        Round.InitialAngle();
+        FragmentsManager.InitialFragments();
         testFragments = new List<BaseFragment>();
         BaseFragment [] baseFragment = new BaseFragment[4];
         for(int i=0;i<4;i++)
@@ -27,5 +33,40 @@ public class RoundPanel : BasePanel
         baseFragment[3].model = FragmentModel.oneHundredAndTwenty;
         testFragments.Add(baseFragment[3]);
         FragmentsManager.UpdateFragments(testFragments);
+    }
+    /// <summary>
+    /// 初始化函数
+    /// </summary>
+    /// <param name="newFragments">传fragmentList就行了</param>
+    public void InitialRoundPanel(List<BaseFragment> newFragments)
+    {
+        if (FragmentsManager.fragmentsOnRound.Count != 0 && FragmentsManager.fragmentsOnRound != null)
+        {
+            for (int i = FragmentsManager.fragmentsOnRound.Count - 1; i >= 0; i--)
+            {
+                BaseFragment fragment = FragmentsManager.fragmentsOnRound[i];
+                fragment.DurationDecrease();
+                if (fragment.finished)
+                    Destroy(fragment.obj);
+                FragmentsManager.fragmentsOnRound.RemoveAt(i);
+            }
+        }
+        gameObject.SetActive(true);
+        if(ifFirstTime)
+        {
+            ifFirstTime = false;
+            Round.InitialAngle();
+            FragmentsManager.InitialFragments();
+        }
+        FragmentsManager.UpdateFragments(newFragments);
+    }
+    /// <summary>
+    /// 结束函数
+    /// </summary>
+    /// <returns></returns>
+    public List<BaseFragment> HideRoundPanel()
+    {
+        gameObject.SetActive(false);
+        return FragmentsManager.fragmentsOnRound;
     }
 }
