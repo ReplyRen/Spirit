b63ffccdca19831e0ed3a4c8af29224eb89f2995
Synthesis-016 <72910142+Synthesis-016@users.noreply.github.com>
2021-02-04 23:23:11 +0800

    各设施对应script，目前实现了简单的购买功能

---
+++ Demo/Assets/Script/Panel/UIPanel/PurchasePanel.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchasePanel : MonoBehaviour
+{
+    public List<BaseObject> storageObject = new List<BaseObject>();
+    public List<BaseObject> purchaseObject = new List<BaseObject>();
+    public GameObject buy;
+    public GameObject panel;
+    public GameObject fatherObj;
+    //更新物品list
+    public void UpdateList()
+    {
+        purchaseObject.Clear();
+        for (int i = 0; i < 10; i++)
+        {
+            BaseObject card = new BaseObject();
+            card.description = i.ToString();
+            card.name = i.ToString();
+            card.sprite = Resources.Load<Sprite>("Sprite/120");
+            purchaseObject.Add(card);
+        }
+        /* List<BaseObject> newList = GameObject.Find("Main Camera").GetComponent<GameManager>().baseList;
+         foreach (var element in newList)
+             storageObject.Add(element);*/
+    }
+    public void Clear()
+    {
+        for (int i = 0; i < fatherObj.transform.childCount; i++)
+            Destroy(fatherObj.transform.GetChild(i).gameObject);
+    }
+    public void UpdateObjects()
+    {
+        if (!buy.GetComponent<UIObject>().isConfirm)
+        {
+            Clear();
+            for (int i = 0; i < purchaseObject.Count; i++)
+            {
+                InstantiateObj(purchaseObject[i].sprite, purchaseObject[i].name);
+            }
+        }
+    }
+    public void Purchase()
+    {
+        for(int i=0;i<purchaseObject.Count;i++)
+        {
+            if (fatherObj.transform.GetChild(i).GetComponent<UIObject>().isUse)
+                storageObject.Add(purchaseObject[i]);
+        }
+        buy.GetComponent<UIObject>().isConfirm = true;
+        buy.SetActive(false);
+    }
+
+    public void Select()
+    {
+        if (!buy.GetComponent<UIObject>().isConfirm)
+        {
+            var btn = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+            btn.GetComponent<UIObject>().isUse = !btn.GetComponent<UIObject>().isUse;
+            btn.GetComponent<Outline>().enabled = !btn.GetComponent<Outline>().enabled;
+        }
+    }
+    public void InstantiateObj(Sprite sprite, string name)
+    {
+        GameObject obj = new GameObject();
+        obj.AddComponent<UIObject>();
+        Button btn = obj.AddComponent<Button>();
+        Image img = obj.AddComponent<Image>();
+        Outline outLine = obj.AddComponent<Outline>();
+        outLine.enabled = false;
+        outLine.effectDistance = new Vector2(5, -5);
+        outLine.effectColor = new Color(0, 0, 0, 1);
+        img.sprite = sprite;
+        obj.name = name;
+        obj.transform.SetParent(fatherObj.transform);
+        btn.targetGraphic = img;
+        btn.onClick.AddListener(Select);
+        obj.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+    }
+
+    void Start()
+    {
+        storageObject = GameObject.Find("FactoryPanel").transform.Find("StoragePanel").GetComponent<StoragePanel>().storageObject;
+        UpdateList();
+        UpdateObjects();
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Demo/Assets/Script/Panel/UIPanel/StoragePanel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoragePanel : MonoBehaviour
+{
+    public List<BaseObject> storageObject = new List<BaseObject>();
+    public GameObject panel;
+    public GameObject fatherObj;
+    public Text text;
+    //更新物品list
+    public void UpdateList()
+    {
+        storageObject.Clear();
+        for (int i = 0; i < 10; i++)
+        {
+            BaseObject card = new BaseObject();
+            card.description = i.ToString();
+            card.name = i.ToString();
+            card.sprite = Resources.Load<Sprite>("Sprite/30");
+            storageObject.Add(card);
+        }
+       /* List<BaseObject> newList = GameObject.Find("Main Camera").GetComponent<GameManager>().baseList;
+        foreach (var element in newList)
+            storageObject.Add(element);*/
+    }
+    public void InstantiateObj(Sprite sprite,string name)
+    {
+        GameObject obj = new GameObject();
+        Button btn = obj.AddComponent<Button>();
+        Image img = obj.AddComponent<Image>();
+        Outline outLine = obj.AddComponent<Outline>();
+        outLine.enabled = false;
+        outLine.effectDistance = new Vector2(5, -5);
+        img.sprite = sprite;
+        obj.name = name;
+        obj.transform.SetParent(fatherObj.transform);
+        btn.targetGraphic = img;
+        btn.onClick.AddListener(ShowDescription);
+        obj.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+    }
+    //更新物品
+    public void Clear()
+    {
+        for (int i = 0; i < fatherObj.transform.childCount; i++)
+            Destroy(fatherObj.transform.GetChild(i).gameObject);
+    }
+    public void UpdateObjects()
+    {
+        Clear();
+        for (int i=0;i<storageObject.Count;i++)
+        {
+            InstantiateObj(storageObject[i].sprite, storageObject[i].name);
+        }
+        Debug.Log(storageObject.Count);
+    }
+    //显示描述
+    public void ShowDescription()
+    {
+        var btn = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+        text.text = storageObject[btn.transform.GetSiblingIndex()].description;
+    }
+    void Start()
+    {
+        text = GameObject.Find("FactoryPanel").transform.Find("StoragePanel/Description").GetComponent<Text>();
+        UpdateList();
+        UpdateObjects();
+        gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        //Debug.Log(storageObject.Count);
+    }
+}
