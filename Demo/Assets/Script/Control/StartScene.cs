f297ca06f1f403917e6a49d6e2d341c5b39cfef8
ReplyRen <ryphy0420@gmail.com>
2021-02-18 13:22:32 +0800

    开始界面

---
+++ Demo/Assets/Script/Control/StartScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
