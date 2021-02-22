9d8cb17e3c86a8db2536b6a90fcae7e5ca93d7e1
ReplyRen <ryphy0420@gmail.com>
2021-02-22 18:58:49 +0800

    图表完成

---
+++ Demo/Assets/Script/Panel/Histogram.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Histogram : MonoBehaviour
+{
+    GameObject pillar;
+    private void Start()
+    {
+        pillar = GameObject.Find("Pillar");
+        pillar.SetActive(false);
+        Test();
+    }
+    private void Test()
+    {
+        Inclusion a = new Inclusion("1", 1);
+        Inclusion b = new Inclusion("2", 0.5f);
+        Inclusion c = new Inclusion("3", 0.01f);
+        Init(a, b, c);
+    }
+    public void Init(params Inclusion[] inclusions)
+    {
+        SetLength(inclusions);
+    }
+    private void SetLength(params Inclusion[] inclusions)
+    {
+        for (int i = 0; i < inclusions.Length; i++)
+        {
+            GameObject obj = Instantiate(pillar, transform);
+            obj.SetActive(true);
+            obj.transform.localPosition = new Vector2(-145 + 60 * i, -190f);
+            obj.GetComponent<RectTransform>().sizeDelta = new Vector2(12, 365 * inclusions[i].value);
+            obj.GetComponentInChildren<Text>().text = inclusions[i].name;
+
+        }
+    }
+    public void UpdateLength(params Inclusion[] inclusions)
+    {
+        SetLength(inclusions);
+    }
+}
---
+++ Demo/Assets/Script/Panel/LineChart.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineChart : MonoBehaviour
+{
+    GameObject point;
+    List<Vector3> pointPos = new List<Vector3>();
+    private void Start()
+    {
+        point = GameObject.Find("Point");
+        point.SetActive(false);
+        Test();
+    }
+    private void Test()
+    {
+        Queue<float> que = new Queue<float>();
+        que.Enqueue(0.3f);
+        que.Enqueue(0.6f);
+        que.Enqueue(1f);
+        que.Enqueue(0f);
+        que.Enqueue(0.51f);
+        que.Enqueue(0.16f);
+        Init(que);
+    }
+    public void Init(Transform parent,Vector3 localPos, Queue<float> nums)
+    {
+        gameObject.transform.SetParent(parent);
+        gameObject.transform.localPosition = localPos;
+        SetPoint(nums);
+        DrawLine();
+    }
+    public void Init(Queue<float> nums)
+    {
+        SetPoint(nums);
+        DrawLine();
+    }
+    private void SetPoint(Queue<float> nums)
+    {
+        int count = nums.Count;
+        for (int i = 0; i < count; i++)
+        {
+            GameObject obj = Instantiate(point, gameObject.transform);
+            obj.SetActive(true);
+            obj.transform.localPosition = new Vector2(-145 + 60 * i, -188 + 362 * nums.Dequeue());
+            pointPos.Add(obj.transform.localPosition);
+        }
+    }
+    private void DrawLine()
+    {
+        LineRenderer lineRenderer = gameObject.GetComponentInChildren<LineRenderer>();
+        lineRenderer.useWorldSpace = false;
+        lineRenderer.startWidth = 0.05f;
+        lineRenderer.endWidth = 0.05f;
+        lineRenderer.startColor = Color.black;
+        lineRenderer.endColor = Color.black;
+        Debug.Log(pointPos.Count);
+        for (int i = 0; i < pointPos.Count; i++) 
+        {
+            lineRenderer.SetPosition(i, pointPos[i]);
+        }
+    }
+}
