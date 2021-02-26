15a2c9022ee216aaa355e815ecdaef470a0fce28
bli23-01 <hust_hjj@126.com>
2021-02-26 22:36:36 +0800

    修正

---
+++ Demo/Assets/Script/Round/FragmentsManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentsManager
+{
+    private static List<BaseFragment> fragments;
+    public static List<BaseFragment> fragmentsOnRound;
+    /// <summary>
+    /// 清零碎片列
+    /// </summary>
+    public static void ClearFragments()
+    {
+        fragments.Clear();
+    }
+    public static void InitialFragments()
+    {
+        fragments = new List<BaseFragment>();
+        fragmentsOnRound = new List<BaseFragment>();
+    }
+    /// <summary>
+    /// 获得碎片列
+    /// </summary>
+    /// <returns></returns>
+    public static List<BaseFragment> GetFragments()
+    {
+        return fragments;
+    }
+    /// <summary>
+    /// 获得特定碎片
+    /// </summary>
+    /// <param name="index">序列号</param>
+    /// <returns></returns>
+    public static BaseFragment GetFragment(int index)
+    {
+        return fragments[index];
+    }
+
+    /// <summary>
+    /// 更新碎片列
+    /// </summary>
+    /// <param name="newFragments"></param>
+    public static void UpdateFragments(List<BaseFragment> newFragments)
+    {
+        if (fragments != null || fragments.Count != 0)  
+            ClearFragments();
+        foreach (var fragment in newFragments)
+        {
+            fragments.Add(fragment);
+        }
+            
+    }
+    /// <summary>
+    /// 返回确认时候圆盘上的碎片
+    /// </summary>
+    /// <returns></returns>
+    /*public static List<BaseFragment> RetrueFragments()
+    {
+        //return FragmentsManager.GetFragments();
+    }*/
+}
---
+++ Demo/Assets/Script/Round/Round.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round
+{
+    /// <summary>
+    /// 120个格子的状态
+    /// 0--无
+    /// 1--满
+    /// 2--边缘1
+    /// 3--边缘2
+    /// </summary>
+    public static int[] angleState;
+
+    public static void InitialAngle()
+    {
+        angleState = new int[120];
+        for (int i = 0; i < 120; i++)
+            angleState[i] = 0;
+    }
+    /// <summary>
+    /// 给出中心线位置判断是否可嵌入
+    /// 0--不嵌入
+    /// 1--可嵌入
+    /// </summary>
+    /// <param name="index">中心线对应位置序号</param>
+    /// <param name="fragmentModel">该碎片是什么样式</param>
+    /// <returns></returns>
+    public static int PlaceRight(int index,FragmentModel fragmentModel)
+    {
+        int totalAngle = ((int)fragmentModel + 1) * 10;
+        for (int i = 0; i <= totalAngle; i++) 
+        {
+            if (angleState[(index - totalAngle / 2 + i + 120) % 120] == 1) 
+                return 0;
+        }
+        return 1;
+    }
+    /// <summary>
+    /// 放置碎片
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="fragmentModel"></param>
+    public static void PutFragment(int index, FragmentModel fragmentModel)
+    {
+        int totalAngle = ((int)fragmentModel + 1) * 10;
+        for (int i = 0; i <= totalAngle; i++)
+        {
+            angleState[(index - totalAngle / 2 + i + 120) % 120] = 1;
+            if (i == 0 || i == totalAngle)
+                angleState[(index - totalAngle / 2 + i + 120) % 120] = 2;
+        }
+    }
+    /// <summary>
+    /// 移出碎片
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="fragmentModel"></param>
+    public static void RemoveFragment(int index, FragmentModel fragmentModel)
+    {
+        int totalAngle = ((int)fragmentModel + 1) * 10;
+        for (int i = 0; i <= totalAngle; i++)
+        {
+            angleState[(index - totalAngle / 2 + i + 120) % 120] = 0;
+        }
+    }
+}
