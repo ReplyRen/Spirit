e240b3c2de7c5be49b85240a8c97f3d680264527
ReplyRen <ryphy0420@gmail.com>
2021-03-11 14:06:08 +0800

    评价图表修改

--- Demo/Assets/Script/Panel/EvaluationChart.cs
+++ Demo/Assets/Script/Panel/EvaluationChart.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class EvaluationChart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    List<Vector2> points = new List<Vector2>();
+    Vector2 center = new Vector2(-4.4f, -16.3f);
+    Vector2 left = new Vector2(-222.3f, 58.5f);
+    Vector2 up = new Vector2(-2.8f, 224.7f);
+    Vector2 right = new Vector2(224.7f, 61.7f);
+    Vector2 downLeft = new Vector2(-140.4f, -202.4f);
+    Vector2 downRight = new Vector2();
+    private void Init(Evaluation obj)
     {
         
     }
