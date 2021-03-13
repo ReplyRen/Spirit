759fa536c6b12e25a8ab9d984b64f417d89cd59f
bli23-01 <hust_hjj@126.com>
2021-03-13 20:14:17 +0800

    声音

---
+++ Demo/Assets/Script/Control/ClickListener.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class ClickListener : MonoBehaviour
+{
+	List<RaycastResult> list = new List<RaycastResult>();
+
+	// Update is called once per frame
+	void Update()
+	{
+		/// 鼠标左键没有点击，就不执行判断逻辑
+		if (!Input.GetMouseButtonDown(0))
+		{
+			return;
+		}
+
+		///相应的GameObject对象
+		GameObject go = null;
+
+		///判断是否点再ui上
+		if (EventSystem.current.IsPointerOverGameObject())
+		{
+			go = ClickUI();
+		}
+		else
+		{
+			go = ClickScene();
+		}
+
+		if (go == null)
+		{
+			Debug.Log("Click Nothing");
+		}
+		else
+		{
+			if (go.TryGetComponent<Button>(out Button button))
+				AudioManager.PlayEffectMusic("ButtonTemp");
+		}
+		
+	}
+
+	/// <summary>
+	/// 点中ui
+	/// </summary>
+	private GameObject ClickUI()
+	{
+		//场景中的EventSystem
+
+		PointerEventData eventData = new PointerEventData(EventSystem.current);
+
+		//鼠标位置
+		eventData.position = Input.mousePosition;
+
+		//调用所有GraphicsRacaster里面的Raycast，然后内部会进行排序，
+		//直接拿出来，取第一个就可以用了
+		EventSystem.current.RaycastAll(eventData, list);
+
+		//这个函数抄的unity源码的，就是取第一个值
+		var raycast = FindFirstRaycast(list);
+
+		//获取父类中事件注册接口
+		//如Button，Toggle之类的，毕竟我们想知道哪个Button被点击了，而不是哪张Image被点击了
+		//当然可以细分为IPointerClickHandler, IBeginDragHandler之类细节一点的，各位可以自己取尝试
+		var go = ExecuteEvents.GetEventHandler<IEventSystemHandler>(raycast.gameObject);
+
+		//既然没拿到button之类的，说明只有Image挡住了，取点中结果即可
+		if (go == null)
+		{
+			go = raycast.gameObject;
+		}
+		return go;
+
+
+	}
+
+	/// <summary>
+	/// Return the first valid RaycastResult.
+	/// </summary>
+	private RaycastResult FindFirstRaycast(List<RaycastResult> candidates)
+	{
+		for (var i = 0; i < candidates.Count; ++i)
+		{
+			if (candidates[i].gameObject == null)
+				continue;
+
+			return candidates[i];
+		}
+		return new RaycastResult();
+	}
+
+	/// <summary>
+	/// 点中场景中对象
+	///
+	/// </summary>
+	private GameObject ClickScene()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit))
+		{
+			GameObject go = hit.collider.gameObject;
+			return go;
+		}
+
+		return null;
+	}
+}
